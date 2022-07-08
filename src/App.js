1c6a89863e40d15a9bb87fdf834a0622acabf745
hckrbaby <taiselize@gmail.com>
2022-07-08 13:55:41 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is{" "}
-        <a
-          href="https://github.com/hacker-baby/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-source
-        </a>{" "}
-        and built by Tais Elize.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is{" "}
+          <a
+            href="https://github.com/hacker-baby/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source
+          </a>{" "}
+          and built by Tais Elize.
+        </footer>
+      </div>
     </div>
   );
 }
