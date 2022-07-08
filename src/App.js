8cda73e789128a619584605eea246ae7ad1a3f8d
hckrbaby <taiselize@gmail.com>
2022-07-08 13:49:21 +0200

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is{" "}
+        <a
+          href="https://github.com/hacker-baby/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-source
+        </a>{" "}
+        and built by Tais Elize.
+      </footer>
+    </div>
+  );
 }
-
-export default App;
