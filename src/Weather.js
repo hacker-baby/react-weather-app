5cd1e6e294f701b57b46a7f47a22b12d196cff20
hckrbaby <taiselize@gmail.com>
2022-07-12 08:29:49 +0200

    Updated icon url

--- src/Weather.js
+++ src/Weather.js
@@ -11,7 +11,7 @@ export default function Weather(props) {
       ready: true,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
